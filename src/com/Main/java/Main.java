3b677283eb2cb554eb80e22b16d9cde933cece29
Lucky123-create <gouripattapu.mamatha@gmail.com>
2022-05-24 11:55:46 +0530

    initial commit

---
+++ src/com/Main/java/Main.java
@@ -0,0 +1,9 @@
+package com.Main.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the session");
+        System.out.println("Hope you are able to follow.");
+    }
+}
